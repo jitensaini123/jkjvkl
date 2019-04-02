8921dde031b6084fadbc21b08a5f704c8fd37665
jitensaini123 <45889988+jitensaini123@users.noreply.github.com>
2019-04-01 17:51:28 -0700

    Initial commit

---
+++ app/src/main/java/jordan/saini/rezubipur/jkjvkl/MainActivity.java
@@ -0,0 +1,13 @@
+package jordan.saini.rezubipur.jkjvkl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
